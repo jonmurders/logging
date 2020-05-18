31d2d526152a64c6678130556e80db8b49a248fc
Jon Murders <jonmurders@localhost.localdomain>
2020-05-18 16:49:24 -0500

    Dredge Specifications Layout

---
+++ dredgespec.py
@@ -0,0 +1,15 @@
+#Process Logging Data into Production Format 
+
+#Dredge Specifications
+class rsweeks:
+    length = 280.0              #ft
+    length_to_trunnion = 280.0  #ft
+    width = 65.0                #ft
+    depth = 17.5                #ft
+    draft = 10.5                #ft
+    freeboard = 7.0             #ft
+    recess_in_hull = 0.0        #ft
+    height_above_deck = 6.0     #ft
+    
+class rsweekslong:
+    length = 280
---
+++ logging.py
@@ -0,0 +1,4 @@
+import dredgespec
+test = dredgespec.rsweeks('length')
+test2 = dredgespec.rsweekslong.length
+print(test2)
